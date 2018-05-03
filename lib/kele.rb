0bca234c2267a28bee3ca44bc3f109da62795643
jbag123 <joshcone@googlemail.com>
2018-05-03 20:21:58 +0100

    commiting url correction

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,7 +2,6 @@ require 'httparty'
 require 'json'
 class Kele
   include HTTParty
-  base_uri 'https://www.bloc.io/api/v1'
   def initialize(email, password)
     response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": email, "password": password })
     @auth_token = response["auth_token"]
@@ -10,8 +9,36 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('https://private-anon-38190ece44-blocapi.apiary-proxy.com/api/v1/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
     stringResponse = response.to_s
     hash = JSON.parse(stringResponse)
+  end
+
+  def get_mentor_availability(mentor_id)
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+    array = []
+    response.each do |r|
+      array.push(r)
+    end
+    return array
+  end
+
+  def get_roadmap(chain_id)
+    response = self.class.get("https://www.bloc.io/api/v1/roadmaps/#{chain_id}", headers: { "authorization" => @auth_token })
+    ruby = JSON.parse(response.body)
+    ruby['sections'].each do |s|
+      puts s['name']
+      s['checkpoints'].each do |c|
+        puts c['name']
+      end
+    end
+  end
+
+  def get_checkpoint(id)
+    response = self.class.get("https://www.bloc.io/api/v1/checkpoints/#{id}", headers: { "authorization" => @auth_token })
+    ruby = JSON.parse(response.body)
+    ruby['sections'][1].each do |s|
+      puts s['name']
+    end
   end
 end
